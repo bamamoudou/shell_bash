fabd49d5a9374155bbffca9b40d759436c841d16
MykouNet <mouradkassa@yahoo.fr>
2019-11-08 11:11:23 +0100

    les condition en bash

---
+++ condition.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ $1 == "mamoudou" ] && [ $2 == "baba" ]
+then
+      echo "Salut mamoudou et baba"
+elif [ $1 = "doro" ]
+then
+echo "salut doro"
+
+elif [ $1 = "marie" ]
+then
+echo "bonjour marie"
+ 
+else
+     echo " $1 t'es qui toi, barres toi"
+fi
